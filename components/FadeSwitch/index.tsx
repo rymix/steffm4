3ce62ff8cba10b5d72f56257004411a65e7aab8d
Steve Arnott <steve.a@smyle.co.uk>
2024-05-30 21:38:28 +0100

    Dynamic color changing is now working

--- components/FadeSwitch/index.tsx
+++ components/FadeSwitch/index.tsx
@@ -5,9 +5,28 @@ import { css, keyframes } from "styled-components";
 
 const generateRandomDuration = (): number => Math.floor(Math.random() * 26) + 5;
 
-function generateRandomDegree(): number {
+const generateRandomDegree = (): number => {
   return Math.floor(Math.random() * 360) + 1;
-}
+};
+
+const colorSets = [
+  ["#dce25e", "#3487f9", "#8ff05e", "#f204ff"],
+  ["#3a52aa", "#5ae409", "#9b61c4", "#3ad1ef", "#7face7", "#4315d1"],
+  ["#bb3886", "#67f0f7", "#ba1354", "#4f9f5c", "#400d4d", "#4dda3a"],
+  ["#8f894d", "#a57e77", "#6461e0", "#dd8354", "#264cea"],
+  ["#747266", "#15bacc", "#f9d298", "#361ac1"],
+  ["#354185", "#fa6d37", "#a5fc8a", "#32ac22", "#b0faa1", "#ff6cf"],
+  ["#a800b5", "#ffaa2e", "#3e65aa", "#925fd4", "#4bdb6b"],
+  ["#b98f8e", "#700972", "#927fc6", "#f73e43", "#6e50e5"],
+  ["#431231", "#d1b048", "#bdd7b9", "#6658ed"],
+  ["#13851b", "#96c78b", "#d0ce03", "#76ad2f", "#752a35"],
+  ["#f5b843", "#3ed219", "#3e6dbf", "#a51e4c"],
+  ["#e9a709", "#a4cc38", "#94924", "#556411", "#7fffeb", "#ee2100"],
+  ["#f3411b", "#82567c", "#c4435e", "#c76312", "#e38408"],
+  ["#eb706c", "#ecfad9", "#58857c"],
+  ["#a74a0b", "#e19e37", "#b9ab41"],
+  ["#11bf19", "#886ae2", "#86811b"],
+];
 
 const FadeSwitch: React.FC = () => {
   const [isFirstGradientVisible, setIsFirstGradientVisible] = useState(true);
@@ -15,6 +34,15 @@ const FadeSwitch: React.FC = () => {
   const [duration2, setDuration2] = useState(0);
   const [degree1, setDegree1] = useState(0);
   const [degree2, setDegree2] = useState(0);
+  const [colors1, setColors1] = useState<string[]>(colorSets[0]);
+  const [colors2, setColors2] = useState<string[]>(colorSets[1]);
+
+  const getRandomColors = (excludeColors: string[]): string[] => {
+    const availableColors = colorSets.filter(
+      (colorSet) => colorSet !== excludeColors,
+    );
+    return availableColors[Math.floor(Math.random() * availableColors.length)];
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -22,9 +50,11 @@ const FadeSwitch: React.FC = () => {
         if (prev) {
           setDegree2(generateRandomDegree());
           setDuration2(generateRandomDuration());
+          setColors2(getRandomColors(colors1));
         } else {
           setDegree1(generateRandomDegree());
           setDuration1(generateRandomDuration());
+          setColors1(getRandomColors(colors2));
         }
         return !prev;
       });
@@ -65,42 +95,37 @@ const FadeSwitch: React.FC = () => {
     ${gradientAnimation2} ${duration}s ease infinite;
   `;
 
-  const gradient1 = (degree: number, duration: number) => css`
-    background: linear-gradient(
-      ${degree}deg,
-      #4a6971,
-      #7146ff,
-      #f48a70,
-      #37f297,
-      #5662fc
-    );
+  const gradient1 = (
+    degree: number,
+    duration: number,
+    colors: string[],
+  ): RuleSet<object> => css`
+    background: linear-gradient(${degree}deg, ${colors.join(", ")});
     background-size: 300% 300%;
     animation: ${animationRule1(duration)};
   `;
 
-  const gradient2 = (degree: number, duration: number) => css`
-    background: linear-gradient(${degree}deg, #e53000, #e2cb5b, #f97827);
+  const gradient2 = (
+    degree: number,
+    duration: number,
+    colors: string[],
+  ): RuleSet<object> => css`
+    background: linear-gradient(${degree}deg, ${colors.join(", ")});
     background-size: 180% 180%;
     animation: ${animationRule2(duration)};
   `;
 
   return (
-    <>
-      <div>duration1: {duration1}</div>
-      <div>duration2: {duration2}</div>
-      <div>degree1: {degree1}</div>
-      <div>degree2: {degree2}</div>
-      <Container>
-        <FadeDiv
-          $isVisible={isFirstGradientVisible}
-          $gradient={gradient1(degree1, duration1)}
-        />
-        <FadeDiv
-          $isVisible={!isFirstGradientVisible}
-          $gradient={gradient2(degree2, duration2)}
-        />
-      </Container>
-    </>
+    <Container>
+      <FadeDiv
+        $isVisible={isFirstGradientVisible}
+        $gradient={gradient1(degree1, duration1, colors1)}
+      />
+      <FadeDiv
+        $isVisible={!isFirstGradientVisible}
+        $gradient={gradient2(degree2, duration2, colors2)}
+      />
+    </Container>
   );
 };
 
