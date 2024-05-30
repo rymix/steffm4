40eb16a0bff4be5441f2de87479bea24324c41c0
Steve Arnott <steve.a@smyle.co.uk>
2024-05-30 21:21:32 +0100

    duration and degree are now automated

--- components/FadeSwitch/index.tsx
+++ components/FadeSwitch/index.tsx
@@ -5,21 +5,35 @@ import { css, keyframes } from "styled-components";
 
 const generateRandomDuration = (): number => Math.floor(Math.random() * 26) + 5;
 
+function generateRandomDegree(): number {
+  return Math.floor(Math.random() * 360) + 1;
+}
+
 const FadeSwitch: React.FC = () => {
   const [isFirstGradientVisible, setIsFirstGradientVisible] = useState(true);
-  const [duration1, setDuration1] = useState(generateRandomDuration());
-  const [duration2, setDuration2] = useState(generateRandomDuration());
+  const [duration1, setDuration1] = useState(0);
+  const [duration2, setDuration2] = useState(0);
+  const [degree1, setDegree1] = useState(0);
+  const [degree2, setDegree2] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIsFirstGradientVisible((prev) => !prev);
+      setIsFirstGradientVisible((prev) => {
+        if (prev) {
+          setDegree2(generateRandomDegree());
+          setDuration2(generateRandomDuration());
+        } else {
+          setDegree1(generateRandomDegree());
+          setDuration1(generateRandomDuration());
+        }
+        return !prev;
+      });
     }, 3000); // Switch every 3 seconds
 
     return () => clearInterval(interval);
   }, []);
 
-  const gradientAnimation1 = css`
-    ${keyframes`
+  const gradientAnimation1 = keyframes`
     0% {
       background-position: 0% 50%;
     }
@@ -29,11 +43,9 @@ const FadeSwitch: React.FC = () => {
     100% {
       background-position: 0% 50%;
     }
-  `}
   `;
 
-  const gradientAnimation2 = css`
-    ${keyframes`
+  const gradientAnimation2 = keyframes`
     0% {
       background-position: 0% 50%;
     }
@@ -43,24 +55,19 @@ const FadeSwitch: React.FC = () => {
     100% {
       background-position: 0% 50%;
     }
-  `}
   `;
 
-  const animationDuration1 = (): RuleSet<object> => css`5s`;
-
-  const animationDuration2 = (): RuleSet<object> => css`15s`;
-
-  const animationRule1 = (): RuleSet<object> => css`
-    ${gradientAnimation1} ${animationDuration1} ease infinite;
+  const animationRule1 = (duration: number): RuleSet<object> => css`
+    ${gradientAnimation1} ${duration}s ease infinite;
   `;
 
-  const animationRule2 = (): RuleSet<object> => css`
-    ${gradientAnimation2} ${animationDuration2} ease infinite;
+  const animationRule2 = (duration: number): RuleSet<object> => css`
+    ${gradientAnimation2} ${duration}s ease infinite;
   `;
 
-  const gradient1 = css`
+  const gradient1 = (degree: number, duration: number) => css`
     background: linear-gradient(
-      123deg,
+      ${degree}deg,
       #4a6971,
       #7146ff,
       #f48a70,
@@ -68,20 +75,32 @@ const FadeSwitch: React.FC = () => {
       #5662fc
     );
     background-size: 300% 300%;
-    animation: ${animationRule1};
+    animation: ${animationRule1(duration)};
   `;
 
-  const gradient2 = css`
-    background: linear-gradient(239deg, #e53000, #e2cb5b, #f97827);
+  const gradient2 = (degree: number, duration: number) => css`
+    background: linear-gradient(${degree}deg, #e53000, #e2cb5b, #f97827);
     background-size: 180% 180%;
-    animation: ${animationRule2};
+    animation: ${animationRule2(duration)};
   `;
 
   return (
-    <Container>
-      <FadeDiv $isVisible={isFirstGradientVisible} $gradient={gradient1} />
-      <FadeDiv $isVisible={!isFirstGradientVisible} $gradient={gradient2} />
-    </Container>
+    <>
+      <div>duration1: {duration1}</div>
+      <div>duration2: {duration2}</div>
+      <div>degree1: {degree1}</div>
+      <div>degree2: {degree2}</div>
+      <Container>
+        <FadeDiv
+          $isVisible={isFirstGradientVisible}
+          $gradient={gradient1(degree1, duration1)}
+        />
+        <FadeDiv
+          $isVisible={!isFirstGradientVisible}
+          $gradient={gradient2(degree2, duration2)}
+        />
+      </Container>
+    </>
   );
 };
 
