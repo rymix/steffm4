2a9d9c5d84784b82e6d7c29efb7ed5deff6e248f
steveasmyle <steve.a@smyle.co.uk>
2024-06-20 10:13:16 +0100

    WIP revised layout

--- components/index.tsx
+++ components/index.tsx
@@ -1,16 +1,60 @@
-import BurgerMenu from "components/BurgerMenu";
-import GradientBackground from "components/GradientBackground";
-import MixCard from "components/MixCard";
-import Mixcloud from "components/Mixcloud";
-import Modal from "components/Modal";
-import Overlay from "components/Overlay";
-import TrackFlow from "components/TrackFlow";
-import TrackSingle from "components/TrackSingle";
-import Vignette from "components/Vignette";
 import { useMixcloud } from "contexts/mixcloud";
 import { useSession } from "contexts/session";
 import { useEffect } from "react";
+import styled from "styled-components";
 import { mcKeyFormatter } from "utils/functions";
+
+import Vignette from "./Vignette";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const TopBlock = styled.div`
+  background: rgba(255, 128, 0, 0.5);
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-height: 30%;
+
+  @media (max-height: 600px) {
+    max-height: 20%;
+  }
+`;
+
+const MiddleBlock = styled.div`
+  background: rgba(255, 255, 0, 0.5);
+  flex: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  > * {
+    width: 100%;
+    height: 100%;
+    max-width: 800px;
+    max-height: 800px;
+    object-fit: contain;
+  }
+`;
+
+const BottomBlock = styled.div`
+  background: rgba(128, 128, 255, 0.5);
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-height: 30%;
+
+  @media (max-height: 600px) {
+    max-height: 20%;
+  }
+`;
 
 const MixcloudHomePage = (): JSX.Element => {
   const {
@@ -41,17 +85,17 @@ const MixcloudHomePage = (): JSX.Element => {
   return (
     <>
       <Vignette />
-      <GradientBackground />
-      <Overlay />
-      <BurgerMenu />
-      <Modal />
+      {/* <GradientBackground /> */}
+      {/* <Overlay /> */}
+      {/* <BurgerMenu /> */}
+      {/* <Modal /> */}
 
       {mcKey && (
-        <>
-          {isMobile ? <TrackSingle /> : <TrackFlow />}
-          <Mixcloud defaultMcKey={mcKey} />
-          <MixCard socials />
-        </>
+        <StyledContainer>
+          <TopBlock>TopBlock</TopBlock>
+          <MiddleBlock>MiddleBlock</MiddleBlock>
+          <BottomBlock>BottomBlock</BottomBlock>
+        </StyledContainer>
       )}
     </>
   );
