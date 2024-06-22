aac3387aad512ab4e5213a0f9a3ada7a8b7a0db5
Steve Arnott <steve.a@smyle.co.uk>
2024-06-22 22:29:30 +0100

    Moved styled to styled component

--- components/Jupiter/Slider/StyledJupiterSlider.ts
+++ components/Jupiter/Slider/StyledJupiterSlider.ts
@@ -1,4 +1,5 @@
 import { Slider } from "@mui/material";
+import JupiterHandle from "public/svg/slider-handle.svg";
 import styled from "styled-components";
 
 export const StyledJupiterSliderWrapper = styled.div`
@@ -13,8 +14,71 @@ export const StyledJupiterSliderWrapper = styled.div`
 `;
 
 export const StyledJupiterSlider = styled(Slider)`
-  .MuiSlider-track {
-    background: blue;
+  & .MuiSlider-track {
     display: none;
+  }
+
+  & .MuiSlider-thumb {
+    width: 28.8px;
+    height: 13.2px;
+    border-radius: 1px;
+    background: url(${JupiterHandle.src}) no-repeat center center;
+    background-size: cover;
+    box-shadow: none;
+    &:focus,
+    &:hover,
+    &:active {
+      box-shadow: "none";
+    }
+  }
+
+  & .MuiSlider-track {
+    width: 8px;
+    background-color: black;
+    position: relative;
+  }
+
+  & .MuiSlider-rail {
+    width: 8px;
+    background-color: black;
+    opacity: 1;
+    position: relative;
+    &::before {
+      content: "";
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 28.8px;
+      height: 100%;
+      background: linear-gradient(
+        to bottom,
+        transparent 0%,
+        black 2%,
+        transparent 3%,
+        transparent 15%,
+        black 17%,
+        transparent 18%,
+        transparent 31%,
+        black 33%,
+        transparent 34%,
+        transparent 48%,
+        black 51%,
+        transparent 52%,
+        transparent 65%,
+        black 67%,
+        transparent 68%,
+        transparent 81%,
+        black 83%,
+        transparent 84%,
+        transparent 96%,
+        black 99%,
+        transparent 100%
+      );
+      pointerevents: none;
+    }
+  }
+
+  & .MuiSlider-root {
+    padding: 0 8px;
   }
 `;
--- components/Jupiter/Slider/index.tsx
+++ components/Jupiter/Slider/index.tsx
@@ -20,66 +20,6 @@ const JupiterSlider: React.FC<JupiterSliderProps> = ({
           defaultValue={70}
           min={0}
           max={100}
-          sx={{
-            "& .MuiSlider-thumb": {
-              width: "28.8px",
-              height: "13.2px",
-              borderRadius: "1px",
-              background: `url(${JupiterHandle.src}) no-repeat center center`,
-              backgroundSize: "cover",
-              boxShadow: "none",
-              "&:focus, &:hover, &:active": {
-                boxShadow: "none",
-              },
-            },
-            "& .MuiSlider-track": {
-              width: "8px",
-              backgroundColor: "black",
-              position: "relative",
-            },
-            "& .MuiSlider-rail": {
-              width: "8px",
-              backgroundColor: "black",
-              opacity: 1,
-              position: "relative",
-              "&::before": {
-                content: '""',
-                position: "absolute",
-                left: "50%",
-                transform: "translateX(-50%)",
-                width: "28.8px",
-                height: "100%",
-                background: `linear-gradient(
-                  to bottom,
-                  transparent 0%,
-                  black 2%,
-                  transparent 3%,
-                  transparent 15%,
-                  black 17%,
-                  transparent 18%,
-                  transparent 31%,
-                  black 33%,
-                  transparent 34%,
-                  transparent 48%,
-                  black 51%,
-                  transparent 52%,
-                  transparent 65%,
-                  black 67%,
-                  transparent 68%,
-                  transparent 81%,
-                  black 83%,
-                  transparent 84%,
-                  transparent 96%,
-                  black 99%,
-                  transparent 100%
-                )`,
-                pointerEvents: "none",
-              },
-            },
-            "& .MuiSlider-root": {
-              padding: "0 8px",
-            },
-          }}
         />
       </StyledJupiterSliderWrapper>
     </>
