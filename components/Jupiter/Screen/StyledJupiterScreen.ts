cd1c3b04212bc0e09feba953f549bbdd0b065ca7
Steve Arnott <steve.a@smyle.co.uk>
2024-06-25 20:41:15 +0100

    Screens tyling

--- components/Jupiter/Screen/StyledJupiterScreen.ts
+++ components/Jupiter/Screen/StyledJupiterScreen.ts
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const StyledJupiterScreenWrapper = styled.div`
   align-items: center;
-  background: yellow;
+  background: #024;
+  background-image: linear-gradient(170deg, #0007, transparent 50%),
+    linear-gradient(to bottom, transparent, #fff1 95%, #fff4 100%);
   display: flex;
   font-family: "dseg14";
-  font-size: clamp(16px, 220%, 34px);
   font-size: 40px;
   font-weight: bold;
   height: 100px;
@@ -14,14 +15,16 @@ export const StyledJupiterScreenWrapper = styled.div`
 `;
 
 export const StyledJupiterScreen = styled.div<{ displayLength: number }>`
+  color: #3af;
   display: flex;
   justify-content: flex-end;
   position: relative;
+  text-shadow: 0 0 0.25em #3af;
 
   &:before {
     content: "${(props) => "~".repeat(props.displayLength)}";
     display: block;
-    opacity: 0.2;
+    opacity: 0.1;
     position: absolute;
     text-shadow: none;
   }
