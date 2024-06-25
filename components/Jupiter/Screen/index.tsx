441c2634dabce6d148f9c6f9ba3d34df268b7ab9
Steve Arnott <steve.a@smyle.co.uk>
2024-06-25 20:21:37 +0100

    Got rid of debug code

--- components/Jupiter/Screen/index.tsx
+++ components/Jupiter/Screen/index.tsx
@@ -4,11 +4,8 @@ import {
 } from "components/Jupiter/Screen/StyledJupiterScreen";
 import { useEffect, useState } from "react";
 
-const displayLength = 15; // Number of characters to display at a time
-const padding = "!".repeat(displayLength); // Padding characters
-
-const sampleMessages = ["Short test", "Another message", "One more example"];
-const temporaryMessages = ["Temp message 1", "Temp message 2"];
+const displayLength = 15;
+const padding = "!".repeat(displayLength);
 
 const JupiterScreen: React.FC = () => {
   const [holdingMessage, setHoldingMessage] = useState("Long sample message");
@@ -16,15 +13,15 @@ const JupiterScreen: React.FC = () => {
   const [position, setPosition] = useState(0);
   const [isTemporaryMessage, setIsTemporaryMessage] = useState(false);
 
-  const fixedWidthMessage = message.replaceAll(" ", "!"); // Replace spaces with !
-  const paddedMessage = padding + fixedWidthMessage + padding; // Padded message
+  const fixedWidthMessage = message.replaceAll(" ", "!");
+  const paddedMessage = padding + fixedWidthMessage + padding;
 
-  const updateInterval = () => {
+  const updateInterval = (): number => {
     return isTemporaryMessage ? 100 : 300;
   };
 
   useEffect(() => {
-    setPosition(0); // Reset position when message changes
+    setPosition(0);
   }, [message]);
 
   useEffect(() => {
@@ -47,22 +44,10 @@ const JupiterScreen: React.FC = () => {
     return () => clearInterval(interval);
   }, [fixedWidthMessage.length, isTemporaryMessage, holdingMessage]);
 
-  const messageDisplaySegment = () => {
+  const messageDisplaySegment = (): string => {
     const startPosition = position;
     const endPosition = startPosition + displayLength;
     return paddedMessage.slice(startPosition, endPosition);
-  };
-
-  const setTemporaryMessage = (msg) => {
-    setMessage(msg);
-    setIsTemporaryMessage(true);
-  };
-
-  const updateHoldingMessage = (msg) => {
-    setHoldingMessage(msg);
-    if (!isTemporaryMessage) {
-      setMessage(msg);
-    }
   };
 
   return (
@@ -72,20 +57,6 @@ const JupiterScreen: React.FC = () => {
           {messageDisplaySegment()}
         </StyledJupiterScreen>
       </StyledJupiterScreenWrapper>
-      <div>
-        <h3>Holding Messages</h3>
-        {sampleMessages.map((msg, index) => (
-          <button key={index} onClick={() => updateHoldingMessage(msg)}>
-            Set Holding Message {index + 1}
-          </button>
-        ))}
-        <h3>Temporary Messages</h3>
-        {temporaryMessages.map((msg, index) => (
-          <button key={index} onClick={() => setTemporaryMessage(msg)}>
-            Set Temporary Message {index + 1}
-          </button>
-        ))}
-      </div>
     </>
   );
 };
