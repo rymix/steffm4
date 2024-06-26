ecb6ac0c59849a50ef655b5d38081ef342ed5fec
steveasmyle <steve.a@smyle.co.uk>
2024-06-26 15:58:28 +0100

    Rear styling WIP

--- components/Jupiter/BackPanel/StyledJupiterBackPanel.ts
+++ components/Jupiter/BackPanel/StyledJupiterBackPanel.ts
@@ -5,10 +5,10 @@ export const StyledJupiterBackPanelWrapper = styled.div`
   zbackground: #3c3c3b;
   zbackground-image: url(${NOISE_BACKGROUND});
   zbackground-size: cover;
-  border-bottom: 2px solid red;
+  border-bottom: 2px solid #6c6c6b;
   height: 50px;
   overflow: hidden;
-  perspective: 400px;
+  perspective: 300px;
   width: 100%;
 `;
 
@@ -19,6 +19,6 @@ export const StyledJupiterBackPanel = styled.div`
   background-position: left;
   height: 100%;
   width: 100%;
-  transform: rotateX(45deg);
+  transform: rotateX(65deg);
   transform-origin: bottom;
 `;
