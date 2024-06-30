dc0ccbbee1512b82c573b12095b21bb15a99f704
Steve Arnott <steve.a@smyle.co.uk>
2024-06-30 21:37:45 +0100

    Hid the gosh darn widget

--- components/Mixcloud/StyledMixcloud.ts
+++ components/Mixcloud/StyledMixcloud.ts
@@ -4,8 +4,8 @@ export const StyledMixcloudWidget = styled.iframe`
   bottom: 0;
   left: 0;
   height: 60px;
-  opacity: 1;
-  position: relative;
+  opacity: 0;
+  position: fixed;
   transform: translateY(60px);
   width: 100%;
   z-index: 1000;
