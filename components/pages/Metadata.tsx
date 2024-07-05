06abab268a0f2fe619f345038ecd5fd7e663bcef
Steve Arnott <steve.a@smyle.co.uk>
2024-07-05 20:18:09 +0100

    Added social sharing meta tags

--- components/pages/Metadata.tsx
+++ components/pages/Metadata.tsx
@@ -12,15 +12,30 @@ const Metadata = (): JSX.Element => {
   } = useMixcloud();
 
   const title =
-    `Stef.FM - ${trackDetails?.trackName} - ${mixDetails?.name}` ||
-    DEFAULT_TITLE;
+    trackDetails?.trackName && mixDetails?.name
+      ? `Stef.FM - ${trackDetails.trackName} - ${mixDetails.name}`
+      : DEFAULT_TITLE;
 
   return (
     <Head>
-      <meta name="description" content={description} />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, user-scalable=no"
+      />
+      <meta name="description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta name="og:title" property="og:title" content={DEFAULT_TITLE} />
+      <meta
+        name="og:description"
+        property="og:description"
+        content={description}
+      />
+      <meta property="og:site_name" content="Stef.FM" />
+      <meta property="og:url" content="https://stef.fm" />
+      <meta
+        name="og:image"
+        property="og:image"
+        content="https://stef.fm/backgrounds/stefmasters-s.png"
       />
       <title>{title}</title>
     </Head>
