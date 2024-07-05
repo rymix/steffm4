24809f46136e24e3bbd3bac0d2b7460f37c6b081
steveasmyle <steve.a@smyle.co.uk>
2024-07-05 08:04:42 +0100

    Adjusted pseudo shadow

--- components/Jupiter/Table/StyledJupiterTable.ts
+++ components/Jupiter/Table/StyledJupiterTable.ts
@@ -17,14 +17,14 @@ export const StyledJupiterTable = styled.div`
   &::after {
     background-color: rgba(255, 255, 255, 0);
     background-image: radial-gradient(
-      circle farthest-side at center,
-      rgba(0, 0, 0, 1) 0%,
+      circle farthest-side at 50% 70%,
+      rgba(0, 0, 0, 0.7) 0%,
       rgba(255, 255, 255, 0) 100%
     );
     bottom: 0;
     content: "";
     display: block;
-    height: 80%;
+    height: 100%;
     left: 0;
     position: absolute;
     width: 100%;
