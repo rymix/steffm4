dfb61a6a3759eddecf5545ee2b4cfe2d6cdf6b1a
steveasmyle <steve.a@smyle.co.uk>
2024-07-07 11:32:58 +0100

    Corrected sort order

--- components/MixList/index.tsx
+++ components/MixList/index.tsx
@@ -1,21 +1,26 @@
 import { CircularProgress } from "@mui/material";
 import MixRow from "components/MixList/MixRow";
 import type { Mix } from "db/types";
+import _ from "lodash";
 import React, { useEffect, useState } from "react";
 
 export const MixList: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [mixes, setMixes] = useState([]);
+  const [mixes, setMixes] = useState<Mix[]>([]);
 
   useEffect(() => {
     const fetchMixes = async (): Promise<void> => {
       try {
         const mixesResponse = await fetch(`/api/mixes`);
         if (!mixesResponse.ok) throw new Error("Data fetch failed");
-        let mixesData = await mixesResponse.json();
-        mixesData = mixesData.sort((a: any, b: any) =>
-          a.name.localeCompare(b.name),
+        let mixesData: Mix[] = await mixesResponse.json();
+
+        mixesData = _.orderBy(
+          mixesData,
+          ["category.code", "listOrder"],
+          ["asc", "asc"],
         );
+
         setMixes(mixesData);
       } catch (error) {
         console.error(error);
