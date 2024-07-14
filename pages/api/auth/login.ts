12e0bb89d16a0ea6704d47b664dfa18186815ef8
Steve Arnott <steve.a@smyle.co.uk>
2024-07-14 21:49:27 +0100

    Added server side logging

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -10,21 +10,35 @@ const users = [
 ];
 
 const handler = (req: NextApiRequest, res: NextApiResponse): void => {
+  console.log("Received request to login");
+  console.log("Request body:", req.body);
+
   const { username, password } = req.body;
+  console.log("Username:", username);
+  console.log("Password:", password);
 
   const user = users.find((u) => u.username === username);
   if (user) {
-    if (bcrypt.compareSync(password, user.password)) {
-      const token = jwt.sign(
-        { username: user.username },
-        process.env.JWT_SECRET!,
-        { expiresIn: "1h" },
-      );
+    console.log("User found:", user);
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+    console.log("Is password correct:", isPasswordCorrect);
+
+    if (isPasswordCorrect) {
+      const secret = process.env.JWT_SECRET!;
+      console.log("JWT Secret:", secret);
+
+      const token = jwt.sign({ username: user.username }, secret, {
+        expiresIn: "1h",
+      });
+      console.log("Generated JWT token:", token);
+
       res.status(200).json({ token });
     } else {
+      console.log("Invalid credentials");
       res.status(401).json({ message: "Invalid credentials" });
     }
   } else {
+    console.log("User not found");
     res.status(401).json({ message: "User not found" });
   }
 };
