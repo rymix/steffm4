ae136deea189b0db75434bcb0d100f56d8c93ae5
Steve Arnott <steve.a@smyle.co.uk>
2024-07-24 22:48:54 +0100

    Added contact content

--- components/Contact/StyledContact.ts
+++ components/Contact/StyledContact.ts
@@ -1,34 +1,40 @@
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import InstagramIcon from "@mui/icons-material/Instagram";
 import styled from "styled-components";
 
+export const StyledEmail = styled(AlternateEmailIcon)`
+  font-size: 2em;
+`;
+
+export const StyledInstagram = styled(InstagramIcon)`
+  font-size: 2em;
+`;
+
+export const StyledSocialsWrapper = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  height: 2em;
+`;
+
 export const StyledContact = styled.div`
-  text-align: left;
-
-  h2 {
-    font-size: 36px;
-    margin: 0 0 20px 0;
-  }
-
-  h3 {
-    font-size: 24px;
-    margin: 20px 0;
-  }
-
-  h4 {
-    font-size: 16px;
-    margin: 0 0 20px 0;
-  }
+  text-align: center;
 
   p {
     line-height: 1.6;
     margin: 0 0 20px 0;
   }
 
-  ul {
-    margin: 0 0 0 20px;
-  }
+  a {
+    color: rgba(0, 0, 0, 0.7);
+    text-decoration: none;
+    font-weight: 600;
+    font-size: 1.2em;
+    transition: 0.3s;
 
-  li {
-    list-style-type: disc;
-    margin: 0 0 20px 0;
+    &:hover {
+      color: rgba(0, 0, 0, 1);
+    }
   }
 `;
--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,11 +1,27 @@
 /* eslint-disable react/no-unescaped-entities */
+import {
+  StyledContact,
+  StyledEmail,
+  StyledInstagram,
+  StyledSocialsWrapper,
+} from "components/Contact/StyledContact";
 import React from "react";
-import { StyledContact } from "./StyledContact";
 
 export const Contact: React.FC = () => {
   return (
     <StyledContact>
-      <h2>Contact</h2>
+      <a href="mailto:webmaster@stef.fm">
+        <StyledSocialsWrapper>
+          <StyledEmail />
+          webmaster@stef.fm
+        </StyledSocialsWrapper>
+      </a>
+      <a href="https://www.instagram.com/stef.fm_music/" target="_blank">
+        <StyledSocialsWrapper>
+          <StyledInstagram />
+          @stef.fm_music
+        </StyledSocialsWrapper>
+      </a>
     </StyledContact>
   );
 };
