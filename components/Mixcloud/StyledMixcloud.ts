fc364f6cd5375fd8bb27187efd12a1889ed84786
Steve Arnott <steve.a@smyle.co.uk>
2025-08-13 15:20:12 +0100

    Hid the mixcloud widget

--- components/Mixcloud/StyledMixcloud.ts
+++ components/Mixcloud/StyledMixcloud.ts
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const StyledMixcloudWidget = styled.iframe`
-  top: 0;
+  /* top: 0;
   left: 0;
   height: 60px;
   opacity: 1;
   position: fixed;
   width: 100%;
-  z-index: 1000000000;
+  z-index: 1000000000; */
 
-  /* bottom: 0;
+  bottom: 0;
   left: 0;
   height: 60px;
   opacity: 0;
   position: fixed;
   width: 100%;
-  z-index: -10000; */
+  z-index: -10000;
 `;
