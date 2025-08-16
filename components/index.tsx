7544f2117a0f1233772b993871b54497be794ecd
Steve Arnott <steve.a@smyle.co.uk>
2025-08-16 18:45:34 +0100

    Refactored Jupiter into its own component

--- components/index.tsx
+++ components/index.tsx
@@ -1,46 +1,21 @@
-import About from "components/About";
-import DrivingMode from "components/DrivingMode";
-import { DiskLabel } from "components/Floppy/types";
-import JupiterButton from "components/Jupiter/Button";
-import JupiterCase from "components/Jupiter/Case";
-import JupiterKnob from "components/Jupiter/Knob";
-import JupiterPanel from "components/Jupiter/Panel";
-import JupiterFrontPanel from "components/Jupiter/Panel/FrontPanel";
-import JupiterSlider from "components/Jupiter/Slider";
-import JupiterTitle from "components/Jupiter/Title";
-import JupiterWrapper from "components/Jupiter/Wrapper";
-import MixInformation from "components/MixInformation";
-import MixList from "components/MixList";
-import OutRun from "components/OutRun";
 import {
   StyledBottomGrid,
   StyledBottomPanel,
   StyledChild,
   StyledChildFloppy,
-  StyledColumn,
   StyledFixedBackground,
   StyledFixedForeground,
-  StyledGridWrapper,
-  StyledItem,
-  StyledItems,
   StyledScrollContainer,
   StyledTopPanel,
 } from "components/Styled";
 import { useMixcloud } from "contexts/mixcloud";
 import { useEffect, useState } from "react";
-import ReactGA from "react-ga4";
-import { useKonami } from "react-konami-code";
-import { GA4, VOLUME_AVAILABLE } from "utils/constants";
-import { getCategoryIndex } from "utils/functions";
-import { useAutoplayInteractionTracking } from "utils/mobileAutoplayHelper";
 
 import Background from "./Background";
 import BurgerMenu from "./BurgerMenu";
 import FloppyDiskStack from "./Floppy/FloppyDiskStack";
-import JupiterHeader from "./Jupiter/Header";
-import JupiterBackPanel from "./Jupiter/Panel/BackPanel";
-import JupiterProgressLeds from "./Jupiter/ProgressLeds";
-import JupiterScreen from "./Jupiter/Screen";
+import { DiskLabel } from "./Floppy/types";
+import JupiterDevice from "./Jupiter";
 import UserManualCover from "./Manual/UserManualCover";
 import Mixcloud from "./Mixcloud";
 import Modal from "./Modal";
@@ -49,204 +24,13 @@ import Overlay from "./Overlay";
 import ScrollIndicator from "./ScrollIndicator";
 import Tooltip from "./Tooltip";
 
-const Jupiter: React.FC = () => {
-  const { trackInteraction } = useAutoplayInteractionTracking();
+const StefFmPlayer: React.FC = () => {
   const {
-    isReady,
     mcKey,
-    controls: {
-      handleLoadRandom,
-      handleLoadRandomFavourite,
-      handlePause,
-      handlePlay,
-      handleNext,
-      handlePrevious,
-    },
-    favourites: { addFavourite, isFavourite, removeFavourite },
-    filters: { categories = [], selectedCategory, setSelectedCategory },
-    mix: { copySharableLink, favourite },
-    session: { openModal },
     track: { details: trackDetails, sectionNumber: trackSectionNumber },
-    widget: { playing, setVolume, volume },
   } = useMixcloud();
-  const [sliderValue, setSliderValue] = useState<number>(volume * 100);
+
   const [diskLabel, setDiskLabel] = useState<DiskLabel>();
-
-  // Helper functions to determine if specific modals are open
-  // Since modalContent is ReactNode, we can't reliably check component types
-  // For now, we'll use a simpler approach - just check if modal is open
-  const isListModalOpen = false; // TODO: Implement proper modal type detection
-  const isInfoModalOpen = false; // TODO: Implement proper modal type detection
-  const isAboutModalOpen = false; // TODO: Implement proper modal type detection
-
-  const easterEgg = (): void => {
-    openModal(<OutRun />, undefined, undefined, true);
-  };
-
-  useKonami(easterEgg);
-
-  const initialKnobValue = selectedCategory
-    ? getCategoryIndex(categories, selectedCategory)
-    : 6;
-
-  const handleSliderChange = (value: number): void => {
-    setSliderValue(value);
-    setVolume(value / 100);
-
-    if (GA4) {
-      ReactGA.event({
-        category: "Option",
-        action: "Slide",
-        label: "Change Volume",
-      });
-    }
-  };
-
-  const handleKnobChange = (index: number): void => {
-    // Track user interaction for autoplay purposes
-    trackInteraction("knob-change");
-
-    const categoryLookup =
-      categories.find((cat) => cat.index === index)?.code || "all";
-
-    if (categoryLookup === "fav") {
-      handleLoadRandomFavourite();
-    } else {
-      handleLoadRandom(categoryLookup);
-    }
-
-    setSelectedCategory(categoryLookup);
-
-    if (GA4) {
-      ReactGA.event({
-        category: "Select",
-        action: "Knob",
-        label: `Change Category ${categoryLookup}`,
-      });
-    }
-  };
-
-  const handleAboutClick = (): void => {
-    openModal(<About />);
-
-    if (GA4) {
-      ReactGA.event({
-        category: "Option",
-        action: "Click",
-        label: "About",
-      });
-    }
-  };
-
-  const handleListClick = (): void => {
-    openModal(<MixList />, undefined, undefined, undefined, true); // Disable shortcuts for search functionality
-
-    if (GA4) {
-      ReactGA.event({
-        category: "Option",
-        action: "Click",
-        label: "Mix List",
-      });
-    }
-  };
-
-  const handleInfoClick = (): void => {
-    openModal(<MixInformation />);
-
-    if (GA4) {
-      ReactGA.event({
-        category: "Option",
-        action: "Click",
-        label: "Mix Information",
-      });
-    }
-  };
-
-  const handleRandomClick = async (): Promise<void> => {
-    // Track user interaction for autoplay purposes
-    trackInteraction("random-button");
-
-    if (selectedCategory === "fav") {
-      handleLoadRandomFavourite();
-    } else if (selectedCategory) {
-      handleLoadRandom(selectedCategory);
-    } else {
-      handleLoadRandom();
-    }
-
-    if (GA4) {
-      ReactGA.event({
-        category: "Option",
-        action: "Click",
-        label: "Random Mix",
-      });
-    }
-  };
-
-  const handleDrivingModeClick = (): void => {
-    openModal(<DrivingMode />);
-
-    if (GA4) {
-      ReactGA.event({
-        category: "Option",
-        action: "Click",
-        label: "Driving Mode",
-      });
-    }
-  };
-
-  // Enhanced media control handlers with interaction tracking
-  const handlePlayWithTracking = (): void => {
-    trackInteraction("play-button");
-    handlePlay();
-  };
-
-  const handlePauseWithTracking = (): void => {
-    trackInteraction("pause-button");
-    handlePause();
-  };
-
-  const handleNextWithTracking = (): void => {
-    trackInteraction("next-button");
-    handleNext();
-  };
-
-  const handlePreviousWithTracking = (): void => {
-    trackInteraction("previous-button");
-    handlePrevious();
-  };
-
-  const handleFavouriteClick = async (): Promise<void> => {
-    if (isFavourite(mcKey)) {
-      removeFavourite(mcKey);
-
-      if (GA4) {
-        ReactGA.event({
-          category: "Option",
-          action: "Click",
-          label: `Favourite Remove ${mcKey}`,
-        });
-      }
-    } else {
-      addFavourite(mcKey);
-
-      if (GA4) {
-        ReactGA.event({
-          category: "Option",
-          action: "Click",
-          label: `Favourite Add ${mcKey}`,
-        });
-      }
-    }
-  };
-
-  const handleShareClick = (): void => {
-    copySharableLink();
-  };
-
-  useEffect(() => {
-    setSliderValue(volume * 100);
-  }, [volume]);
 
   useEffect(() => {
     setDiskLabel({
@@ -272,135 +56,7 @@ const Jupiter: React.FC = () => {
 
       <StyledScrollContainer>
         <StyledTopPanel>
-          <JupiterWrapper>
-            <JupiterCase>
-              <JupiterPanel padding="0" background="rear">
-                <JupiterBackPanel />
-              </JupiterPanel>
-              <JupiterPanel align="right" padding="12">
-                <JupiterHeader />
-              </JupiterPanel>
-              <JupiterPanel padding="24">
-                <JupiterScreen />
-              </JupiterPanel>
-              <JupiterPanel padding="0">
-                <JupiterProgressLeds />
-              </JupiterPanel>
-              <JupiterPanel padding="12">
-                <StyledGridWrapper>
-                  <StyledColumn>
-                    <JupiterTitle title="Select" />
-                    <StyledItems>
-                      <StyledItem>
-                        <JupiterKnob
-                          size={92}
-                          degrees={220}
-                          min={1}
-                          max={6}
-                          value={initialKnobValue}
-                          steps
-                          labelVisible={false}
-                          categories={categories}
-                          onCategoryChange={handleKnobChange}
-                          onChange={() => {}}
-                        />
-                      </StyledItem>
-                    </StyledItems>
-                  </StyledColumn>
-                  <StyledColumn>
-                    <JupiterTitle title="Control" />
-                    <StyledItems>
-                      <StyledItem>
-                        <JupiterButton
-                          color="red"
-                          label="Stop"
-                          onClick={handlePauseWithTracking}
-                          on={playing === false}
-                        />
-                        <JupiterButton
-                          color="green"
-                          label="Play"
-                          onClick={handlePlayWithTracking}
-                          on={playing === true}
-                        />
-                        <JupiterButton
-                          color="cream"
-                          label="Prev"
-                          onClick={handlePreviousWithTracking}
-                          momentary
-                        />
-                        <JupiterButton
-                          color="cream"
-                          label="Next"
-                          onClick={handleNextWithTracking}
-                          momentary
-                        />
-                        <JupiterButton
-                          color="blue"
-                          label="Rand"
-                          onClick={handleRandomClick}
-                          momentary
-                        />
-                        <JupiterButton
-                          color="blue"
-                          label="Car"
-                          onClick={handleDrivingModeClick}
-                          momentary
-                        />
-                      </StyledItem>
-                    </StyledItems>
-                  </StyledColumn>
-                  <StyledColumn>
-                    <JupiterTitle title="Option" />
-                    <StyledItems>
-                      <StyledItem>
-                        <JupiterButton
-                          color="yellow"
-                          label="Fav"
-                          onClick={handleFavouriteClick}
-                          on={favourite}
-                        />
-                        <JupiterButton
-                          color="yellow"
-                          label="Share"
-                          onClick={handleShareClick}
-                          momentary
-                        />
-                        <JupiterButton
-                          color="orange"
-                          label="Info"
-                          onClick={handleInfoClick}
-                          on={isInfoModalOpen}
-                        />
-                        <JupiterButton
-                          color="orange"
-                          label="List"
-                          onClick={handleListClick}
-                          on={isListModalOpen}
-                        />
-                        <JupiterButton
-                          color="orange"
-                          label="About"
-                          onClick={handleAboutClick}
-                          on={isAboutModalOpen}
-                        />
-                        {isReady && VOLUME_AVAILABLE && (
-                          <JupiterSlider
-                            label="Vol"
-                            volume={sliderValue}
-                            onChange={handleSliderChange}
-                          />
-                        )}
-                      </StyledItem>
-                    </StyledItems>
-                  </StyledColumn>
-                </StyledGridWrapper>
-              </JupiterPanel>
-              <JupiterPanel padding="0" background="front">
-                <JupiterFrontPanel />
-              </JupiterPanel>
-            </JupiterCase>
-          </JupiterWrapper>
+          <JupiterDevice />
         </StyledTopPanel>
         <StyledBottomPanel>
           <StyledBottomGrid>
@@ -420,4 +76,4 @@ const Jupiter: React.FC = () => {
   );
 };
 
-export default Jupiter;
+export default StefFmPlayer;
