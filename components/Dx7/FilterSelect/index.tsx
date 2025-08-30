585873d9fed1b5c6bea69e4519bf22e526e10498
Steve Arnott <steve.a@smyle.co.uk>
2025-08-30 21:48:10 +0100

    WIP mobile layout

--- components/Dx7/FilterSelect/index.tsx
+++ components/Dx7/FilterSelect/index.tsx
@@ -1,6 +1,9 @@
 import Dx7Button from "components/Dx7/Button";
 import { StyledDx7FilterSelect } from "components/Dx7/FilterSelect/StyledDx7FilterSelect";
-import { StyledDx7Item } from "components/Dx7/Item/StyledDx7Item";
+import {
+  StyledDx7Item,
+  StyledDx7ItemGroup,
+} from "components/Dx7/Item/StyledDx7Item";
 import { useMixcloud } from "contexts/mixcloud";
 
 const Dx7FilterSelect: React.FC = () => {
@@ -21,72 +24,76 @@ const Dx7FilterSelect: React.FC = () => {
 
   return (
     <StyledDx7FilterSelect>
-      <StyledDx7Item>
-        <Dx7Button
-          color="grey"
-          label="All"
-          onClick={() => {
-            handleFilterSelectChange("all");
-          }}
-          on={selectedCategory === "all"}
-          size="tiny"
-        />
-      </StyledDx7Item>
-      <StyledDx7Item>
-        <Dx7Button
-          color="grey"
-          label="Adv"
-          onClick={() => {
-            handleFilterSelectChange("aidm");
-          }}
-          on={selectedCategory === "aidm"}
-          size="tiny"
-        />
-      </StyledDx7Item>
-      <StyledDx7Item>
-        <Dx7Button
-          color="grey"
-          label="Shoes"
-          onClick={() => {
-            handleFilterSelectChange("mpos");
-          }}
-          on={selectedCategory === "mpos"}
-          size="tiny"
-        />
-      </StyledDx7Item>
-      <StyledDx7Item>
-        <Dx7Button
-          color="grey"
-          label="Cock"
-          onClick={() => {
-            handleFilterSelectChange("cocksoup");
-          }}
-          on={selectedCategory === "cocksoup"}
-          size="tiny"
-        />
-      </StyledDx7Item>
-      <StyledDx7Item>
-        <Dx7Button
-          color="grey"
-          label="Spec"
-          onClick={() => {
-            handleFilterSelectChange("special");
-          }}
-          on={selectedCategory === "special"}
-          size="tiny"
-        />
-      </StyledDx7Item>
-      <StyledDx7Item>
-        <Dx7Button
-          color="grey"
-          label="Fav"
-          onClick={() => {
-            handleFilterSelectChange("fav");
-          }}
-          on={selectedCategory === "fav"}
-          size="tiny"
-        />
-      </StyledDx7Item>
+      <StyledDx7ItemGroup>
+        <StyledDx7Item>
+          <Dx7Button
+            color="grey"
+            label="All"
+            onClick={() => {
+              handleFilterSelectChange("all");
+            }}
+            on={selectedCategory === "all"}
+            size="tiny"
+          />
+        </StyledDx7Item>
+        <StyledDx7Item>
+          <Dx7Button
+            color="grey"
+            label="Adv"
+            onClick={() => {
+              handleFilterSelectChange("aidm");
+            }}
+            on={selectedCategory === "aidm"}
+            size="tiny"
+          />
+        </StyledDx7Item>
+        <StyledDx7Item>
+          <Dx7Button
+            color="grey"
+            label="Shoes"
+            onClick={() => {
+              handleFilterSelectChange("mpos");
+            }}
+            on={selectedCategory === "mpos"}
+            size="tiny"
+          />
+        </StyledDx7Item>
+      </StyledDx7ItemGroup>
+      <StyledDx7ItemGroup>
+        <StyledDx7Item>
+          <Dx7Button
+            color="grey"
+            label="Cock"
+            onClick={() => {
+              handleFilterSelectChange("cocksoup");
+            }}
+            on={selectedCategory === "cocksoup"}
+            size="tiny"
+          />
+        </StyledDx7Item>
+        <StyledDx7Item>
+          <Dx7Button
+            color="grey"
+            label="Spec"
+            onClick={() => {
+              handleFilterSelectChange("special");
+            }}
+            on={selectedCategory === "special"}
+            size="tiny"
+          />
+        </StyledDx7Item>
+        <StyledDx7Item>
+          <Dx7Button
+            color="grey"
+            label="Fav"
+            onClick={() => {
+              handleFilterSelectChange("fav");
+            }}
+            on={selectedCategory === "fav"}
+            size="tiny"
+          />
+        </StyledDx7Item>
+      </StyledDx7ItemGroup>
     </StyledDx7FilterSelect>
   );
 };
--- components/Dx7/Item/StyledDx7Item.ts
+++ components/Dx7/Item/StyledDx7Item.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const StyledDx7ItemGroup = styled.div`
+  background: orange;
+`;
+
 export const StyledDx7Item = styled.div`
   padding: 0 2px;
 `;
