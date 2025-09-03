da32bc1fb9cfa9a997ac497c7fb96a791deb0c7c
Steve Arnott <steve.a@smyle.co.uk>
2025-09-03 19:46:16 +0100

    Driving mode themed buttons

--- components/DrivingMode/index.tsx
+++ components/DrivingMode/index.tsx
@@ -4,6 +4,7 @@ import {
   StyledMixTitle,
   StyledSubtitle,
 } from "components/DrivingMode/StyledDrivingMode";
+import Dx7Button from "components/Dx7/Button";
 import JupiterButton from "components/Jupiter/Button";
 import { useMixcloud } from "contexts/mixcloud";
 import React from "react";
@@ -13,46 +14,51 @@ const DrivingMode: React.FC = () => {
     widget: { playing },
     controls: { handlePlay, handlePause, handleNext, handlePrevious },
     mix: { details: mixDetails },
+    themes: { playerTheme },
   } = useMixcloud();
+
+  const isDx7Theme = playerTheme === "DX7";
+  const ButtonComponent = isDx7Theme ? Dx7Button : JupiterButton;
+  const buttonSize = isDx7Theme ? "huge" : "large";
 
   return (
     <StyledDrivingMode>
       <StyledMixTitle>{mixDetails?.name || "Loading..."}</StyledMixTitle>
 
       <StyledButtonGrid>
-        <JupiterButton
+        <ButtonComponent
           color="red"
           label="Stop"
           onClick={handlePause}
           on={playing === false}
-          size="large"
+          size={buttonSize}
           textColor="black"
         />
 
-        <JupiterButton
+        <ButtonComponent
           color="green"
           label="Play"
           onClick={handlePlay}
           on={playing === true}
-          size="large"
+          size={buttonSize}
           textColor="black"
         />
 
-        <JupiterButton
+        <ButtonComponent
           color="cream"
           label="Prev"
           onClick={handlePrevious}
           momentary
-          size="large"
+          size={buttonSize}
           textColor="black"
         />
 
-        <JupiterButton
+        <ButtonComponent
           color="cream"
           label="Next"
           onClick={handleNext}
           momentary
-          size="large"
+          size={buttonSize}
           textColor="black"
         />
       </StyledButtonGrid>
