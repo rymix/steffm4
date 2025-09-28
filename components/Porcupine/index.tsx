f920153783b4cf587a464a2756d1ec6840b3a0e2
Steve Arnott <steve.a@smyle.co.uk>
2025-09-28 21:36:02 +0100

    Set default keyword

--- components/Porcupine/index.tsx
+++ components/Porcupine/index.tsx
@@ -1,6 +1,6 @@
 import { usePorcupine } from "@picovoice/porcupine-react";
-import { BuiltInKeyword, PorcupineKeyword } from "@picovoice/porcupine-web";
-import React, { useEffect, useRef, useState } from "react";
+import { PorcupineKeyword } from "@picovoice/porcupine-web";
+import React, { useEffect, useState } from "react";
 import { PICOVOICE_KEY } from "utils/constants";
 
 const porcupineModel = {
@@ -8,30 +8,10 @@ const porcupineModel = {
   customWritePath: "3.0.0_porcupine_params.pv",
 };
 
-const porcupineKeywords = [];
-
-if (
-  porcupineKeywords.length === 0 &&
-  porcupineModel.publicPath.endsWith("porcupine_params.pv")
-) {
-  for (const k in BuiltInKeyword) {
-    // @ts-ignore
-    console.log("k", k);
-    porcupineKeywords.push({ builtin: k });
-  }
-  console.log(
-    "--------------------------------------porcupineKeywords",
-    porcupineKeywords,
-  );
-}
+const defaultKeyword = { builtin: "Alexa" } as PorcupineKeyword;
 
 export const Porcupine: React.FC = () => {
   const [keywordDetections, setKeywordDetections] = useState<string[]>([]);
-  const [keyword, setKeyword] = useState<PorcupineKeyword>(
-    porcupineKeywords[0],
-  );
-
-  const accessKeyRef = useRef("");
 
   const {
     keywordDetection,
@@ -44,23 +24,9 @@ export const Porcupine: React.FC = () => {
     release,
   } = usePorcupine();
 
-  console.log("farts");
-
   useEffect(() => {
-    console.log("----------------------keyword", keyword);
-
-    init(PICOVOICE_KEY, [keyword], porcupineModel);
+    init(PICOVOICE_KEY, [defaultKeyword], porcupineModel);
   }, []);
-
-  const setSelectedKeyword = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selected = e.target.value;
-    for (const k of porcupineKeywords) {
-      if (k.label === selected || k.builtin === selected) {
-        setKeyword(k);
-        return;
-      }
-    }
-  };
 
   useEffect(() => {
     const changeKeyword = async (): Promise<void> => {
@@ -78,24 +44,10 @@ export const Porcupine: React.FC = () => {
 
   return (
     <div className="voice-widget">
-      <h2>VoiceWidget</h2>
       <h3>Loaded: {JSON.stringify(isLoaded)}</h3>
       <h3>Listening: {JSON.stringify(isListening)}</h3>
       <h3>Error: {JSON.stringify(error !== null)}</h3>
       {error && <p className="error-message">{error.toString()}</p>}
-      <h3>
-        <label>Keyword: </label>
-        <select
-          value={(keyword as any).label ?? (keyword as any).builtin}
-          onChange={(e) => setSelectedKeyword(e)}
-        >
-          {porcupineKeywords.map((k) => (
-            <option key={k.label ?? k.builtin} value={k.label ?? k.builtin}>
-              {k.label ?? k.builtin}
-            </option>
-          ))}
-        </select>
-      </h3>
       <br />
       <button
         onClick={() => start()}
@@ -122,6 +74,7 @@ export const Porcupine: React.FC = () => {
       {keywordDetections.length > 0 && (
         <ul>
           {keywordDetections.map((label: string, index: number) => (
+            // eslint-disable-next-line react/no-array-index-key
             <li key={index}>{label}</li>
           ))}
         </ul>
